53aac847f11a57f6e01f3dd9cb195df0164888f3
Eduardo Maciel Sanchez <54561697+DesolateElf-dev@users.noreply.github.com>
2020-06-15 00:05:20 -0300

    Add files via upload
    Primeiro upload

---
+++ src/controller/ArquivosController.java
@@ -0,0 +1,118 @@
+package controller;
+
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import javax.swing.JOptionPane;
+
+public class ArquivosController implements IArquivosController{
+
+	public ArquivosController() {
+		super();
+	}
+
+	@Override
+	public void readDir(String path) throws IOException {
+		File dir = new File(path);
+		if (dir.exists() && dir.isDirectory()) { //verifica se existe e verifica se é um diretório
+			File[] files = dir.listFiles(); //retorna um vetor com as files
+			for (File f : files) {
+				if (f.isFile()) {
+					System.out.println("     \t"+f.getName());
+				} else {
+					System.out.println("<DIR>\t"+f.getName());
+				}
+			}
+		} else {
+			throw new IOException("Diretório inválido");
+		}
+		
+	}
+
+	@Override
+	public void createFile(String path, String name) throws IOException {
+		File dir = new File(path); //caminho do diretório
+		//File arq = new File(path, name + ".txt"); //cria o arquivo no diretório 'path' com o nome 'name' formato .txt
+		File arq = new File(path, name); //se quiser criar arquivo .csv
+		if (dir.exists() && dir.isDirectory()) {
+			boolean existe = false;
+			if (arq.exists()) { //verifica se diretório existe
+				existe = true; //verifica se arquivo existe
+			}
+			String conteudo = geraTxt(); //gera o texto
+			FileWriter fileWriter = new FileWriter(arq, existe); //cria o arquivo
+			PrintWriter print = new PrintWriter(fileWriter); //abre o arquivo e define o tipo de operação de escrita
+			print.write(conteudo); //inicia a variável de fazer a escrita e faz a escrita
+			print.flush();
+			print.close();
+			fileWriter.close(); //fecha o arquivo
+		} else {
+			throw new IOException("Diretório Inválido");
+		}
+		
+	}
+
+	private String geraTxt() {
+		StringBuffer buffer = new StringBuffer();
+		String linha = "";
+		while (!linha.equalsIgnoreCase("fim")) { //enquanto a linha não for "fim" faça
+			linha = JOptionPane.showInputDialog(null,"Digite uma frase",
+					"Entrada de texto", JOptionPane.INFORMATION_MESSAGE);
+			if (!linha.equalsIgnoreCase("fim")) {
+				buffer.append(linha+"\r\n");
+			}
+		}
+		return buffer.toString();
+	}
+
+	@Override
+	public void readFile(String path, String name) throws IOException {
+		File arq = new File(path, name);
+		if (arq.exists() && arq.isFile()) { //verifica se existe e verifica se é um diretório
+			FileInputStream fluxo = new FileInputStream(arq); //abre o arquivo
+			InputStreamReader leitor = new InputStreamReader(fluxo); //lê o arquivo
+			BufferedReader buffer = new BufferedReader(leitor); //converte o arquivo
+			String linha = buffer.readLine(); //coloca o arquivo no buffer
+			//faz as operações
+			while (linha != null) { //procura EOF (fim do arquivo)
+				System.out.println(linha);
+				linha = buffer.readLine();
+			}
+			buffer.close();
+			leitor.close();
+			fluxo.close(); //fecha o arquivo
+			
+		} else {
+			throw new IOException("Arquivo inválido");
+		}
+	}
+
+	@Override
+	public void openFile(String path, String name) throws IOException {
+		File arq = new File(path, name);
+		if (arq.exists() && arq.isFile()) {
+			Desktop desktop = Desktop.getDesktop();
+			desktop.open(arq);
+		} else {
+			throw new IOException("Arquivo Inválido");
+		}
+		
+	}
+	
+	
+/*
+	private int id;
+	//ctrl + 3 "contructor" (cria o contrutor) 
+
+	public ArquivosController(int id) {
+		this.id = id;
+	}
+*/
+	
+}
---
+++ src/view/Principal.java
@@ -0,0 +1,48 @@
+package view;
+
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import controller.ArquivosController;
+import controller.IArquivosController;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		IArquivosController arqCont = new ArquivosController();
+		String dirWin = "C:\\Windows";
+		String path = "C:\\temp\\Aula";
+		//String name = "teste.csv"; //criar arquivo .csv
+		//String name = "temp";
+		String name = JOptionPane.showInputDialog(null, "Digite o nome do arquivo com extenção: ",
+				"Entrada de texto", JOptionPane.INFORMATION_MESSAGE);
+		
+		try {
+			int opc=0;
+
+			while(opc!=9) {
+				opc = Integer.parseInt(JOptionPane.showInputDialog("1 - Ler diretório "+dirWin+"\n2- Cria Arquivo  \n3 - Lê arquivos  \n4 - Abre arquivo  \n9 - Finalizar"));
+				switch (opc){
+		    		case 1: arqCont.readDir(dirWin);
+		    			break;
+		    		case 2: arqCont.createFile(path, name);
+		    			break;
+		    		case 3: arqCont.readFile(path, name);
+		    			break;	
+		    		case 4: arqCont.openFile(path, name);
+		    			break;
+		    		case 9: {
+		    			System.out.println("Programa Finalizado");
+		    			System.exit(0);
+		    		}
+		    		default: JOptionPane.showMessageDialog(null,"Inválido");
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
